b869ff92f1caaaa112ff6d7c2f7fc704b440cd83
Luan Campos <luancamposproducoes@gmail.com>
2021-11-11 18:36:39 -0300

    chore: add first migration

---
+++ src/database/migration/1636664548867-createUsers.ts
@@ -0,0 +1,56 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class createUsers1636664548867 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'username',
+            type: 'varchar',
+            length: '20',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'avatar_url',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'password_hash',
+            type: 'varchar',
+            length: '1024',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users')
+    await queryRunner.query('DROP EXTENSION "uuid-ossp"')
+  }
+}
