f64141ac2be44eb7429ba26d95b7d8060d7968f1
Luan Campos <luancamposproducoes@gmail.com>
2021-11-11 19:28:58 -0300

    chore: create users and validations

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,16 +1,31 @@
 import { Request, Response } from 'express'
+import { validationResult } from 'express-validator'
+import { getCustomRepository } from 'typeorm'
+
+import UserRepository from '../database/repositories/UserRepository'
 
 class UserController {
   async store(req: Request, res: Response) {
-    const { username, name, email, avatar_url, password_hash } = req.body
+    const userRepository = getCustomRepository(UserRepository)
 
-    const user = {
+    const { username, name, email, avatar_url, password_hash } = req.body
+    const errors = validationResult(req)
+    if (!errors.isEmpty())
+      return res.status(400).json({ error: errors.array()[0].msg })
+
+    const UserExist = await userRepository.findOne({ username })
+
+    if (UserExist) return res.status(400).json({ error: 'Usuário já existe!' })
+
+    const user = userRepository.create({
       username,
       name,
       email,
       avatar_url,
       password_hash,
-    }
+    })
+
+    await userRepository.save(user)
 
     res.status(201).json(user)
   }
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,28 @@
-import { Request, Response, Router } from 'express'
+import { Router } from 'express'
+import { body } from 'express-validator'
+
 import UserController from './controllers/UserController'
 
 const routes = Router()
 
 // Users
-routes.post('/users', UserController.store)
+routes.post(
+  '/users',
+  body('username')
+    .matches(/^@[A-Za-z0-9]+([._][A-Za-z0-9]+)*$/)
+    .toLowerCase()
+    .withMessage(
+      'Nome de usuário deve iniciar com @ e ter no mínimo 2 caracteres!'
+    ),
+  body('name')
+    .matches(/[A-Z][a-z]* [A-Z][a-z]*/)
+    .withMessage('Por favor preencha nome e sobrenome!'),
+  body('email').isEmail().withMessage('Endereço de email inválido!'),
+  body('avatar_url').isURL().withMessage('Url da imagem é inválido!'),
+  body('password_hash')
+    .isLength({ min: 8 })
+    .withMessage('A senha deve conter conter no mínimo 8 digitos!'),
+  UserController.store
+)
 
 export default routes
