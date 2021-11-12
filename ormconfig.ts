792a56c4fc04c2e4594e1446b80307afc04a2a1f
Luan Campos <luancamposproducoes@gmail.com>
2021-11-12 15:57:21 -0300

    ci: update config

---
+++ ormconfig.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+
+export default {
+  type: process.env.TYPE_DB,
+  host: process.env.HOST_DB,
+  port: process.env.PORT_DB,
+  username: process.env.USERNAME_DB,
+  password: process.env.PASSWORD_DB,
+  database: process.env.DATABASE_DB,
+  synchronize: true,
+  logging: false,
+  entities: ['src/database/entity/**/*.ts'],
+  migrations: ['src/database/migration/**/*.ts'],
+  subscribers: ['src/database/subscriber/**/*.ts'],
+  cli: {
+    entitiesDir: 'src/database/entity',
+    migrationsDir: 'src/database/migration',
+    subscribersDir: 'src/database/subscriber',
+  },
+}
