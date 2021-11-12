11af95ca4a87543a59b0f30980ba5b8c1e588824
Luan Campos <luancamposproducoes@gmail.com>
2021-11-12 17:08:37 -0300

    chore: add permissionsRoles migration

---
+++ src/database/migration/1636746828719-createPermissionsRoles.ts
@@ -0,0 +1,57 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm'
+
+export class createPermissionsRoles1636746828719 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    await queryRunner.createTable(
+      new Table({
+        name: 'permissions_roles',
+        columns: [
+          { name: 'role_id', type: 'uuid' },
+          { name: 'permission_id', type: 'uuid' },
+        ],
+      })
+    )
+
+    await queryRunner.createForeignKey(
+      'permissions_roles',
+      new TableForeignKey({
+        columnNames: ['permission_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'permissions',
+        name: 'fk_permissions_roles_',
+        onDelete: 'CASCADE',
+        onUpdate: 'SET NULL',
+      })
+    )
+
+    await queryRunner.createForeignKey(
+      'permissions_roles',
+      new TableForeignKey({
+        columnNames: ['role_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'roles',
+        name: 'fk_roles_permissions_',
+        onDelete: 'CASCADE',
+        onUpdate: 'SET NULL',
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey(
+      'permissions_roles',
+      'fk_roles_permissions_'
+    )
+
+    await queryRunner.dropForeignKey(
+      'permissions_roles',
+      'fk_permissions_roles_'
+    )
+  }
+}
