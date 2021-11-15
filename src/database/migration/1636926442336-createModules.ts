af42789116a4516a0712a2b5039409dfc2a7d406
Luan Campos <luancamposproducoes@gmail.com>
2021-11-15 16:16:33 -0300

    chore: update migrations

---
+++ src/database/migration/1636926442336-createModules.ts
@@ -0,0 +1,47 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class createModules1636926442336 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    await queryRunner.createTable(
+      new Table({
+        name: 'modules',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+            length: '255',
+            isUnique: true,
+          },
+          {
+            name: 'avatar_url',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('modules')
+    await queryRunner.query('DROP EXTENSION "uuid-ossp"')
+  }
+}
